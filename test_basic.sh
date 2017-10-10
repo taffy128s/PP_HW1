55fc83bc3e71dcea6c06d5507e83dcb38f6d413d
Taffy Cheng <taffy128s@gmail.com>
2017-10-10 14:38:21 +0800

    formatted

--- test_basic.sh
+++ test_basic.sh
@@ -2,15 +2,15 @@ rm -f output*
 pass=1
 for i in $(seq -f "%02g" 1 10)
 do
-	printf "\n\n-----Now testing No.$i...\n"
-	time srun -N 1 -n 1 -p batch ./HW1_103062122_basic 0 ./samples/testcase$i output$i
-	x=$(diff ./samples/sorted$i output$i)
-	time srun -N 1 -n 2 -p batch ./HW1_103062122_basic 0 ./samples/testcase$i output$i
-	y=$(diff ./samples/sorted$i output$i)
+  printf "\n\n-----Now testing No.$i...\n"
+  time srun -N 1 -n 1 -p batch ./HW1_103062122_basic 0 ./samples/testcase$i output$i
+  x=$(diff ./samples/sorted$i output$i)
+  time srun -N 1 -n 2 -p batch ./HW1_103062122_basic 0 ./samples/testcase$i output$i
+  y=$(diff ./samples/sorted$i output$i)
   time srun -N 1 -n 4 -p batch ./HW1_103062122_basic 0 ./samples/testcase$i output$i
-	a=$(diff ./samples/sorted$i output$i)
-	time srun -N 1 -n 8 -p batch ./HW1_103062122_basic 0 ./samples/testcase$i output$i
-	b=$(diff ./samples/sorted$i output$i)
+  a=$(diff ./samples/sorted$i output$i)
+  time srun -N 1 -n 8 -p batch ./HW1_103062122_basic 0 ./samples/testcase$i output$i
+  b=$(diff ./samples/sorted$i output$i)
   if [[ $x == "" ]] && [[ $y == "" ]] && [[ $a == "" ]] && [[ $b == "" ]]
   then 
     pass=$(($pass & 1))
